0282bc00a2ba5944e73b2826cbee4886b525afc9
= <=>
2018-03-08 17:16:07 +0700

    setup library

--- Development/source/app/src/main/java/com/example/wilson/humancharacteristics/MainActivity.java
+++ Development/source/app/src/main/java/com/example/wilson/humancharacteristics/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.wilson.humancharacteristics;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
