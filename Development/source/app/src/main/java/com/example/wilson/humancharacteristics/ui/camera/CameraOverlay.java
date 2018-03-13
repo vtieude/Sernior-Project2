636cf65d9ce4a9628c7ce0d76ac7c4c48e41b051
= <=>
2018-03-13 16:24:49 +0700

    change .gitignore

---
+++ Development/source/app/src/main/java/com/example/wilson/humancharacteristics/ui/camera/CameraOverlay.java
@@ -0,0 +1,10 @@
+package com.example.wilson.humancharacteristics.ui.camera;
+
+import android.view.View;
+
+/**
+ * Created by enclaveit on 13/03/2018.
+ */
+
+public class CameraOverlay extends View {
+}
---
+++ Development/source/app/src/main/java/com/example/wilson/humancharacteristics/ui/camera/CameraSourcePreview.java
@@ -0,0 +1,10 @@
+package com.example.wilson.humancharacteristics.ui.camera;
+
+import android.view.ViewGroup;
+
+/**
+ * Created by enclaveit on 13/03/2018.
+ */
+
+public class CameraSourcePreview extends ViewGroup {
+}
