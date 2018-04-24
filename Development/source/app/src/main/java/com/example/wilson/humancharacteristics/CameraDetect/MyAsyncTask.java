c918076ad37d9148a4c6737b1dffa8de394d11d5
Wilson <wilson@enclave.vn>
2018-04-24 16:36:42 +0700

    change myasynctask

--- Development/source/app/src/main/java/com/example/wilson/humancharacteristics/CameraDetect/MyAsyncTask.java
+++ Development/source/app/src/main/java/com/example/wilson/humancharacteristics/CameraDetect/MyAsyncTask.java
@@ -8,14 +8,13 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.wilson.humancharacteristics.R;
-import com.example.wilson.humancharacteristics.bean.HumanModel;
 import com.example.wilson.humancharacteristics.model.FaceResult;
 
 /**
  * Created by Wilson on 4/24/2018.
  */
 
-public class MyAsyncTask extends AsyncTask<FaceResult[], Void, Void > {
+public class MyAsyncTask extends AsyncTask<FaceResult, FaceResult, FaceResult[]> {
     public Activity activityScren;
     public ProgressBar progress;
     public MyAsyncTask(Activity activity) {
@@ -23,33 +22,38 @@ public class MyAsyncTask extends AsyncTask<FaceResult[], Void, Void > {
     }
 
     @Override
-    protected Void doInBackground(FaceResult[]... faceResults) {
-        for (int i = 0; i < faceResults.length; i ++) {
-            Toast.makeText(activityScren, i,
-                    Toast.LENGTH_SHORT).show();
-        }
-        return null;
-    }
-
-    @Override
     protected void onPreExecute() {
-        // TODO Auto-generated method stub
         super.onPreExecute();
         progress = (ProgressBar)activityScren.findViewById(R.id.progressBarCamera);
         progress.setVisibility(View.VISIBLE);
-        Toast.makeText(activityScren, "Saving!",
-                Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    protected void onProgressUpdate(Void... values) {
+    protected FaceResult[] doInBackground(FaceResult... faceResults) {
+        for (int i = 0; i < faceResults.length; i++) {
+            SystemClock.sleep(100);
+            if (faceResults[i].getId() != 0) {
+
+            }
+        }
+
+        return faceResults;
+    }
+
+    @Override
+    protected void onProgressUpdate(FaceResult... values) {
         super.onProgressUpdate(values);
-
+        if (values[0].getId() != 0) {
+            Toast.makeText(activityScren, "Savingrer!" + values[0].getAttractive(),
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
-    protected void onPostExecute(Void aVoid) {
-        super.onPostExecute(aVoid);
+    protected void onPostExecute(FaceResult[] faceResults) {
+        super.onPostExecute(faceResults);
+        Toast.makeText(activityScren, "Savingrer!" + faceResults.length,
+                Toast.LENGTH_SHORT).show();
         progress.setVisibility(View.INVISIBLE);
     }
 }
