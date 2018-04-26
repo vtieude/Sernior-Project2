ca2d13826a7ac8ed4ad2f8d7d074d0b9d35e5251
= <=>
2018-04-26 16:58:47 +0700

    feature/dave/UpdateUI fix rotate

--- Development/source/app/src/main/java/com/example/wilson/humancharacteristics/CameraDetect/CameraDetectActivity.java
+++ Development/source/app/src/main/java/com/example/wilson/humancharacteristics/CameraDetect/CameraDetectActivity.java
@@ -352,6 +352,7 @@ public final class CameraDetectActivity extends AppCompatActivity implements Sur
         mDisplayOrientation = Util.getDisplayOrientation(mDisplayRotation, cameraId);
 
         mCamera.setDisplayOrientation(mDisplayOrientation);
+
         if (mFaceView != null) {
             mFaceView.setDisplayOrientation(mDisplayOrientation);
         }
--- Development/source/app/src/main/java/com/example/wilson/humancharacteristics/utils/Util.java
+++ Development/source/app/src/main/java/com/example/wilson/humancharacteristics/utils/Util.java
@@ -52,7 +52,7 @@ public class Util {
         int result;
         if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
             result = (info.orientation + degrees) % 360;
-            result = (360 - result) % 360;  // compensate the mirror
+                result = (360 - result) % 360;  // compensate the mirror
         } else {  // back-facing
             result = (info.orientation - degrees + 360) % 360;
         }
